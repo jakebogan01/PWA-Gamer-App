ed870bd198a07b3d13830eaa2f4b0dce0a798928
Jake Bogan <jakebogan01@gmail.com>
2022-10-26 13:53:46 -0400

    adds slider section

---
+++ src/components/MiddleNav.js
@@ -0,0 +1,34 @@
+import {Link} from "react-router-dom";
+import {Component} from "react";
+
+export default class MiddleNav extends Component {
+    render() {
+        return (
+            <section>
+                <div className="px-[1.5625rem] pt-[1.5625rem]">
+                    <span className="font-semibold text-[0.5625rem] text-[#D9D9D9]">Let’s Explore</span>
+                    <h1 className="font-bold text-[2.5rem] leading-none">Best Games</h1>
+                </div>
+
+                <nav className="px-[1.5625rem] mt-[1.625rem]">
+                    <ul className="flex items-center justify-between font-semibold text-sm">
+                        <li>
+                            <Link to="/">Popular</Link>
+                        </li>
+                        <li>
+                            <Link to="/" className="text-[#AC7784]">Newest</Link>
+                        </li>
+                        <li>
+                            <Link to="/" className="text-[#AC7784]">Recommended</Link>
+                        </li>
+                        <li>
+                            <Link to="/">
+                                <svg width="18" height="18" fill="#ffffff" xmlns="http://www.w3.org/2000/svg"><path d="M7.644 0a7.652 7.652 0 0 0-2.359 14.925 7.646 7.646 0 0 0 6.857-1.087l3.81 3.81a1.2 1.2 0 1 0 1.696-1.697l-3.808-3.81a7.646 7.646 0 0 0-1.073-10.184A7.64 7.64 0 0 0 7.644 0Zm0 12.9a5.247 5.247 0 0 1-5.248-5.25A5.25 5.25 0 0 1 7.644 2.4a5.247 5.247 0 0 1 3.709 8.96 5.253 5.253 0 0 1-3.71 1.54Z"/></svg>
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            </section>
+        );
+    }
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,35 +1,13 @@
-import {Link} from "react-router-dom";
-import user from '../assets/images/profile-pic.png';
+import TopNav from "../components/TopNav";
+import MiddleNav from "../components/MiddleNav";
+import Popular from "../components/Popular";
 
 const Home = () => {
     return (
         <div>
-            <header className="pt-[2.25rem] px-[1.5625rem]">
-                <nav className="flex items-center justify-between">
-                    <ul className="flex items-center space-x-3">
-                        <li className="flex">
-                            <Link to="/" className="inline-block w-[3.125rem]">
-                                <img src={user} className="w-full" alt="user profile" />
-                            </Link>
-                        </li>
-                        <li>
-                            <span className="block font-semibold text-[0.5625rem]">Happy Weekend</span>
-                            <span className="block font-semibold text-2xl leading-none">Hi, Vitaily</span>
-                        </li>
-                    </ul>
-
-                    <Link to="" className="flex justify-center items-center w-[3.125rem] h-[3.125rem] border rounded-full" style={{borderColor: "rgba(59, 35, 39, 32%)"}}>
-                        <svg width="18" height="18" fill="#ffffff" xmlns="http://www.w3.org/2000/svg"><path d="M3.687 0C1.644 0 0 1.586 0 3.555v.36C0 5.885 1.644 7.47 3.687 7.47h.805c2.043 0 3.255-1.168 3.255-3.138v-.777C7.747 1.585 6.102 0 4.059 0h-.372ZM13.94 0c-2.043 0-3.688 1.586-3.688 3.555v.777c0 1.97 1.212 3.138 3.255 3.138h.805C16.355 7.47 18 5.884 18 3.915v-.36C18 1.585 16.355 0 14.312 0h-.372ZM3.687 9.886C1.644 9.886 0 11.472 0 13.442v.359c0 1.97 1.644 3.555 3.687 3.555h.372c2.043 0 3.688-1.585 3.688-3.555v-.777c0-1.97-1.212-3.138-3.255-3.138h-.805Zm9.82 0c-2.042 0-3.254 1.169-3.254 3.138v.777c0 1.97 1.645 3.555 3.687 3.555h.373c2.042 0 3.687-1.586 3.687-3.555v-.36c0-1.969-1.645-3.555-3.687-3.555h-.806Z"/></svg>
-                    </Link>
-                </nav>
-            </header>
-
-            <section>
-                <div className="px-[1.5625rem] pt-[1.5625rem]">
-                    <span className="font-semibold text-[0.5625rem] text-[#D9D9D9]">Let’s Explore</span>
-                    <h1 className="font-bold text-[2.5rem] leading-none">Best Games</h1>
-                </div>
-            </section>
+            <TopNav />
+            <MiddleNav/>
+            <Popular/>
         </div>
     );
 }
